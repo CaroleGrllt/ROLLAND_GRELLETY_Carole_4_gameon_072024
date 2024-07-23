c40a501aa7e6a055255d39580570c4f27bcd8c71
Carole ROLLAND GRELLETY <carole.grellety@gmail.com>
2024-07-23 14:32:53 +0200

    Ouverture / fermeture 1ere modale

--- starterOnly/assets/js/modal.js
+++ starterOnly/assets/js/modal.js
@@ -8,16 +8,38 @@ function editNav() {
 }
 
 // DOM Elements
-const modalbg = document.querySelector(".bground");
-const modalBtn = document.querySelectorAll(".modal-btn");
+// ---- première modale
+
+const modalBtn = document.querySelectorAll(".btn-signup");
+const modalBg = document.querySelector(".bground");
+const content = document.querySelector(".content");
+const closeBtnFirst = document.querySelector(".modalfirst-close-btn");
 const formData = document.querySelectorAll(".formData");
 
-// launch modal event
+// ---- deuxième modale
+const modalBgConfirm = document.querySelector(".bground-modal");
+const contentConfirm = document.querySelector(".modal-content");
+const closeBtnSecond = document.querySelector(".modalsecond-close-btn");
+const closeModalBtn = document.querySelector(".btn-close");
+
+
+// Ouverture de la première modale
 modalBtn.forEach((btn) => btn.addEventListener("click", launchModal));
 
-// launch modal form
 function launchModal() {
-  modalbg.style.display = "block";
+  modalBg.style.display = "block";
+  content.classList.remove("close-modal");
 }
+
+// Fermeture de la première modale
+
+closeBtnFirst.addEventListener("click", closeModal);
+
+function closeModal() {
+  content.classList.add("close-modal");
+  modalBg.style.display = 'none'
+}
+
+// validation du formulaire
 
 
