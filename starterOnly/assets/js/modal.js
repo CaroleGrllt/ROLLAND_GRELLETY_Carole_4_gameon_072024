8da06b77ad49b2006acf0b031c955d1c2b757b52
Carole ROLLAND GRELLETY <carole.grellety@gmail.com>
2024-08-03 17:44:46 +0200

    Optimisation des erreurs : utilisation des data-error

--- starterOnly/assets/js/modal.js
+++ starterOnly/assets/js/modal.js
@@ -9,31 +9,20 @@ function editNav() {
 
 // DOM ELEMENTS
 // ---- première modale : ouverture / fermeture
-
 const modalBtn      = document.querySelectorAll(".btn-signup");
 const modalBg       = document.querySelector(".bground");
 const content       = document.querySelector(".content");
 const closeBtnFirst = document.querySelector(".modalfirst-close-btn");
 
 // ---- première modale : éléments formulaire
-const formData              = document.querySelectorAll(".formData");
 const firstInput            = document.getElementById("first");
-const firstInputError       = document.getElementById("error-message-first");
 const lastInput             = document.getElementById("last");
-const lastInputError        = document.getElementById("error-message-last");
 const emailInput            = document.getElementById("email");
-const emailInputError       = document.getElementById("error-message-email");
 const birthdateInput        = document.getElementById("birthdate");
-const birthdateInputError   = document.getElementById("error-message-birthdate");
 const quantityInput         = document.getElementById("quantity");
-const quantityInputError    = document.getElementById("error-message-quantity");
 const locationInputs        = document.querySelectorAll('input[name="location"]');
-const locationInputsSpan    = document.querySelectorAll('.checkbox-icon')
-const locationInputsError   = document.getElementById("error-message-location");
 const checkboxInput         = document.getElementById("checkbox1");
-const checkboxInputError    = document.getElementById("error-message-checkbox");
 const checkboxInput2        = document.getElementById("checkbox2");
-
 
 // ---- deuxième modale : ouverture / fermeture
 const modalBgConfirm  = document.querySelector(".bground-modal");
@@ -44,10 +33,7 @@ const closeModalBtn   = document.querySelector(".btn-close");
 
 
 //FONCTIONS 
-
 // ---- Ouverture de la première modale
-modalBtn.forEach((btn) => btn.addEventListener("click", launchModal));
-
 function launchModal() {
   modalBg.style.display = "block";
   content.classList.remove("close-modal");
@@ -67,8 +53,6 @@ function clearInputs() {
   checkboxInput2.checked = false
 }
 
-closeBtnFirst.addEventListener("click", closeModal);
-
 function closeModal() {
   content.classList.add("close-modal");
   modalBg.style.display = 'none'
@@ -82,179 +66,130 @@ function displaySecondModal() {
 }
 
 // ---- Fermeture de la seconde modale (clic croix)
-closeBtnSecond.addEventListener("click", closeSecondModal);
-closeModalBtn.addEventListener("click", closeSecondModal);
-
 function closeSecondModal() {
   contentConfirm.classList.add("close-modal");
   modalBgConfirm.style.display = 'none'
 }
 
-
 // ---- validation du formulaire : vérification des données entrées
 // --------prénom
-function isValidFirstname(input, error) {
+function isValidFirstname(input) {
   const firstnameRegexp = /^[A-Za-zÀ-ÖØ-öø-ÿ\s'-]{2,}$/;
+  const firstError = document.querySelector(".first")
   if (!firstnameRegexp.test(input.value) || input ==="") {
-    error.style.display = "block";
-    input.classList.add("erreur-input");
+    firstError.setAttribute('data-error-visible', 'true')
     console.log("false prénom")
     return false;
   } else {
-    error.style.display = "none";
-    input.classList.remove("erreur-input");
+    firstError.setAttribute('data-error-visible', 'false')
     console.log('true prénom')
     return true;
   }
 }
 
 // --------nom
-
-function isValidLastname(input, error) {
+function isValidLastname(input) {
   const lastnameRegexp = /^[A-Za-zÀ-ÖØ-öø-ÿ\s'-]{1,}$/;
+  const lastError = document.querySelector(".last")
   if (!lastnameRegexp.test(input.value) || input ==="") {
-    error.style.display = "block";
-    input.classList.add("erreur-input");
+    lastError.setAttribute('data-error-visible', 'true')
     console.log("false nom")
     return false;
   } else {
-    error.style.display = "none";
-    input.classList.remove("erreur-input");
+    lastError.setAttribute('data-error-visible', 'false')
     console.log('true nom')
     return true;
   }
 }
 
 // --------email
-
-function isValidEmail(input, error) {
+function isValidEmail(input) {
   const emailRegexp = /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/;
+  const emailError = document.querySelector(".email")
   if (!emailRegexp.test(input.value) || input ==="") {
-    error.style.display = "block";
-    input.classList.add("erreur-input");
+    emailError.setAttribute('data-error-visible', 'true')
     console.log("false email")
     return false;
   } else {
-    error.style.display = "none";
-    input.classList.remove("erreur-input");
+    emailError.setAttribute('data-error-visible', 'false')
     console.log('true email')
     return true;
   }
 }
 
 // --------date
-function isValidDateOfBirth(input, error) {
+function isValidDateOfBirth(input) {
 
   const dateRegex = /^(19|20)\d\d-(0[1-9]|1[0-2])-(0[1-9]|[12][0-9]|3[01])$/;
   const dateOfBirth = new Date(input.value);
   const today = new Date();
+  const birthdayError = document.querySelector(".birthdate")
 
   if (!dateRegex.test(input.value) || dateOfBirth >= today || input === "") {
-    error.style.display = "block";
-    input.classList.add("erreur-input");
+    birthdayError.setAttribute('data-error-visible', 'true')
     console.log("false regex, futur ou vide")
     return false;
   }
-
+  birthdayError.setAttribute('data-error-visible', 'false')
   console.log('true date')
-  error.style.display = "none";
-  input.classList.remove("erreur-input");
   return true;
 }
 
 // --------Quantité
-
-function isValidQuantity(input, error) {
+function isValidQuantity(input) {
   const numberRegexp = /^(?:[0-9]|[1-9][0-9])$/; // le nombre doit être compris entre 0 et 99
-  
+  const quantityError = document.querySelector(".quantity")
   if(input==="" || isNaN(input.value) || !numberRegexp.test(input.value)) {
-    error.style.display = "block";
-    input.classList.add("erreur-input");
+    quantityError.setAttribute('data-error-visible', 'true')
     console.log("false quantité")
     return false;
   } else {
-    error.style.display = "none";
-    input.classList.remove("erreur-input");
+    quantityError.setAttribute('data-error-visible', 'false')
     console.log('true quantité')
     return true;
   }
 }
 
 // --------Radios
-
-function isRadioSelected(locationInputs, error) {
+function isRadioSelected(locationInputs) {
   console.log(locationInputs)
+  const radioError = document.querySelector(".tournoi-choix")
     for(let i = 0; i< locationInputs.length; i++) {
       if(locationInputs[i].checked) {
-        error.style.display = "none";
+        radioError.setAttribute('data-error-visible', 'false')
         console.log('true radio')
         return true;
       } 
     }
-
-    error.style.display = "block";
+    radioError.setAttribute('data-error-visible', 'true')
     console.log("false radio")
     return false;
 }
 
 // --------Checkbox
-
-function isCheckboxChecked(input, error) {
+function isCheckboxChecked(input) {
+  const cguError = document.querySelector(".cgu")
   if(!input.checked) {
-    error.style.display = "block";
-    input.classList.add("erreur-input");  
+    cguError.setAttribute('data-error-visible', 'true')
     console.log("false checkbox")
     return false
   }
-  error.style.display = "none";
-  input.classList.remove("erreur-input");
+  cguError.setAttribute('data-error-visible', 'false')
   console.log("true checkbox")
   return true;  
   
 }
 
-// ---- validation du formulaire : les eventlistener
-
-firstInput.addEventListener("change", function() {
-  isValidFirstname(firstInput, firstInputError);
-});
-
-lastInput.addEventListener("change", function() {
-  isValidLastname(lastInput, lastInputError);
-});
-
-emailInput.addEventListener("change", function() {
-  isValidEmail(emailInput, emailInputError);
-});
-
-birthdateInput.addEventListener("change", function() {
-  isValidDateOfBirth(birthdateInput, birthdateInputError);
-});
-
-quantityInput.addEventListener("change", function() {
-  isValidQuantity(quantityInput, quantityInputError);
-});
-
-locationInputs.forEach((locationInput) =>{
-  locationInput.addEventListener("change", function() {
-    isRadioSelected(locationInputs, locationInputsError);
-  })
-});
-
-checkboxInput.addEventListener("change", function() {
-  isCheckboxChecked(checkboxInput, checkboxInputError);
-});
 
 // ---- validation du formulaire
-
 function validate() {
-  const validFirstname   = isValidFirstname(firstInput, firstInputError);
-  const validLastname    = isValidLastname(lastInput, lastInputError);
-  const validEmail       = isValidEmail(emailInput, emailInputError);
-  const validDateOfBirth = isValidDateOfBirth(birthdateInput, birthdateInputError);
-  const validQuantity    = isValidQuantity(quantityInput, quantityInputError);
-  const selectedRadio    = isRadioSelected(locationInputs, locationInputsError);
-  const checkedCheckbox  = isCheckboxChecked(checkboxInput, checkboxInputError);
+  const validFirstname   = isValidFirstname(firstInput);
+  const validLastname    = isValidLastname(lastInput);
+  const validEmail       = isValidEmail(emailInput);
+  const validDateOfBirth = isValidDateOfBirth(birthdateInput);
+  const validQuantity    = isValidQuantity(quantityInput);
+  const selectedRadio    = isRadioSelected(locationInputs);
+  const checkedCheckbox  = isCheckboxChecked(checkboxInput);
 
   function isValidForm() {
     if(validFirstname && validLastname && validEmail && validDateOfBirth && validQuantity && selectedRadio && checkedCheckbox) {
@@ -279,3 +214,35 @@ function validate() {
     return false
   }
 }
+
+// EVENTLISTENERS : Modales et formulaire
+// --------Modales
+modalBtn.forEach((btn) => btn.addEventListener("click", launchModal));
+closeBtnFirst.addEventListener("click", closeModal);
+closeBtnSecond.addEventListener("click", closeSecondModal);
+closeModalBtn.addEventListener("click", closeSecondModal);
+
+// --------Formulaire
+firstInput.addEventListener("change", function() {
+  isValidFirstname(firstInput);
+});
+lastInput.addEventListener("change", function() {
+  isValidLastname(lastInput);
+});
+emailInput.addEventListener("change", function() {
+  isValidEmail(emailInput);
+});
+birthdateInput.addEventListener("change", function() {
+  isValidDateOfBirth(birthdateInput);
+});
+quantityInput.addEventListener("change", function() {
+  isValidQuantity(quantityInput);
+});
+locationInputs.forEach((locationInput) =>{
+  locationInput.addEventListener("change", function() {
+    isRadioSelected(locationInputs);
+  })
+});
+checkboxInput.addEventListener("change", function() {
+  isCheckboxChecked(checkboxInput);
+});
